aa9174202d7fae7f6a71e8564353c6301fd9deb8
yuxiaohong <1351130147@qq.com>
2025-03-26 18:57:26 +0800

    2025.3.26

--- Exercise/src/easy/_14.java
+++ Exercise/src/easy/_14.java
@@ -1,4 +1,32 @@
 package easy;
 
+import java.util.Objects;
+
 public class _14 {
+
+    public static void main(String[] args) {
+        _14 test = new _14();
+        System.out.println(test.longestCommonPrefix(new String[]{""}));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            prefix = compare(prefix, strs[i]);
+        }
+        return prefix;
+    }
+
+    public String compare(String str1, String str2) {
+        int len = Math.min(str1.length(), str2.length());
+        String res = "";
+        for (int i = 0; i < len; i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                break;
+            } else {
+                res = res + str1.charAt(i);
+            }
+        }
+        return res;
+    }
 }
---
+++ Exercise/src/easy/_35.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class _35 {
+    public static void main(String[] args) {
+        _35 test = new _35();
+        test.searchInsert(new int[]{1, 2, 3, 4, 5}, 3);
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        for (int num : nums) {
+            
+        }
+
+        return 0;
+    }
+}
