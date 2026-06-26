71420de2060a28d56f13427132d7e295960b2e72
yuhong <yuhong@shinet.cn>
2026-06-26 20:59:12 +0800

    144-二叉树的先序遍历 145-二叉树的后序遍历

---
+++ Exercise/src/easy/_144BinaryTreePreorderTraversal.java
@@ -0,0 +1,40 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _144BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        this.preorder(root, list);
+        return list;
+    }
+
+    public void preorder(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) return;
+        list.add(root.val);
+        this.preorder(root.left, list);
+        this.preorder(root.right, list);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
