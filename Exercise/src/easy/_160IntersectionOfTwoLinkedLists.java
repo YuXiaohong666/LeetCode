46f409b7163c3de9a0bc35736574c79e5c434217
yuhong <yuhong@shinet.cn>
2026-06-26 21:51:16 +0800

    160-交叉链表

--- Exercise/src/easy/_160IntersectionOfTwoLinkedLists.java
+++ Exercise/src/easy/_160IntersectionOfTwoLinkedLists.java
@@ -1,8 +1,43 @@
 package easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class _160IntersectionOfTwoLinkedLists {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        return null;
+        Set<ListNode> set = new HashSet<>();
+        ListNode flagNode = null;
+        while (true) {
+            if (headA == null && headB == null) break;
+            if (headA != null) {
+                if (!set.add(headA)) {
+                    flagNode = headA;
+                    break;
+                }
+                headA = headA.next;
+            }
+            if (headB != null) {
+                if (!set.add(headB)) {
+                    flagNode = headB;
+                    break;
+                }
+                headB = headB.next;
+            }
+        }
+        return flagNode;
+    }
+
+    //    优化解法：双指针换头法
+    public ListNode instantFunc(ListNode headA, ListNode headB) {
+        ListNode pA = headA;
+        ListNode pB = headB;
+
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+
+        return pA;
     }
 
     public static class ListNode {
