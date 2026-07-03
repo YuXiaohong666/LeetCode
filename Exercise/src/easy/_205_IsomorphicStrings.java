325a8854467b14321ebc8916c1733e924b1fae65
yuhong <yuhong@shinet.cn>
2026-07-03 20:59:03 +0800

    205. 同构字符串

--- Exercise/src/easy/_205_IsomorphicStrings.java
+++ Exercise/src/easy/_205_IsomorphicStrings.java
@@ -1,7 +1,27 @@
 package easy;
 
-public class _205_IsomorphicStrings {
-    public boolean isIsomorphic(String s, String t) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class _205_IsomorphicStrings {
+    public static void main(String[] args) {
+        _205_IsomorphicStrings obj = new _205_IsomorphicStrings();
+        System.out.println(obj.isIsomorphic("badc", "baba"));
+    }
+
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i))) {
+                if (map.containsValue(t.charAt(i))) {
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+            }
+            if (!map.get(s.charAt(i)).equals(t.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
