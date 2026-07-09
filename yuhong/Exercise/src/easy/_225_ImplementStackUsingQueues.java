ef126723e71170d1316652f7f72c62ec52c09d67
yuhong <yuhong@shinet.cn>
2026-07-09 15:34:11 +0800

    新增成员目录

---
+++ yuhong/Exercise/src/easy/_225_ImplementStackUsingQueues.java
@@ -0,0 +1,31 @@
+package easy;
+
+public class _225_ImplementStackUsingQueues {
+    public static void main(String[] args) {
+        // MyStack obj = new MyStack();
+        // obj.push(x);
+        // int param_2 = obj.pop();
+        // int param_3 = obj.top();
+        // boolean param_4 = obj.empty();
+    }
+
+    // public MyStack() {
+    //
+    // }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+        return 0;
+    }
+
+    public int top() {
+        return 0;
+    }
+
+    public boolean empty() {
+        return true;
+    }
+}
